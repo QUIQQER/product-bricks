9767118991034da5a89f0b8050cf86db7265e8c7
michael.daniel <michael.danielczok@gmail.com>
2018-03-26 16:40:21 +0200

    feat: You can choose products to show in the slides. refactor: "Buy now" button translation. refactor: Code tidied up.

--- src/QUI/ProductBricks/Controls/Slider/ProductSlider.php
+++ src/QUI/ProductBricks/Controls/Slider/ProductSlider.php
@@ -7,7 +7,6 @@
 namespace QUI\ProductBricks\Controls\Slider;
 
 use QUI;
-use QUI\Projects\Media\Utils;
 use QUI\ERP\Products\Handler\Products;
 
 
@@ -29,15 +28,13 @@ class ProductSlider extends QUI\Control
 
         // default options
         $this->setAttributes([
-            'title'          => '',
-            'text'           => '',
-            'class'          => 'quiqqer-productbricks-productslider',
-            'nodeName'       => 'section',
-            'shownavigation' => true,
-            'showarrows'     => 'showHoverScale',
-            'autostart'      => false,
-            'delay'          => 5000,
-            'template'       => dirname(__FILE__) . '/ProductSlider.html'
+            'title'     => '',
+            'text'      => '',
+            'class'     => 'quiqqer-productbricks-productslider',
+            'nodeName'  => 'section',
+            'autostart' => false,
+            'delay'     => 5000,
+            'template'  => dirname(__FILE__) . '/ProductSlider.html'
         ]);
 
         $this->addCSSFile(dirname(__FILE__) . '/ProductSlider.css');
@@ -47,27 +44,40 @@ class ProductSlider extends QUI\Control
     {
         $Engine = QUI::getTemplateManager()->getEngine();
         $Slider = new QUI\Bricks\Controls\Slider\Promoslider([
-            'shownavigation' => $this->getAttribute('shownavigation'),
+            'shownavigation' => true,
             'showarrows'     => $this->getAttribute('showHoverScale'),
             'autostart'      => $this->getAttribute('autostart'),
             'delay'          => $this->getAttribute('delay'),
-            'template' => dirname(__FILE__) . '/ProductSlider.Template.html'
+            'template'       => dirname(__FILE__) . '/ProductSlider.Template.html'
         ]);
-
-
 
         $this->setStyle('background-color', $this->getAttribute('bgColor'));
         $this->setStyle('background-image', 'url(' . $this->getAttribute('bgImage') . ')');
 
-        $products = Products::getProducts([
-            'limit' => 5
-        ]);
+        $productIds = $this->getAttribute('productIds');
+        $productIds = explode(',', $productIds);
+
+        $products = [];
+
+        foreach ($productIds as $productId) {
+            try {
+                $Product    = Products::getProduct($productId);
+                $products[] = $Product->getView();
+            } catch (QUI\Exception $Exception) {
+                QUI\System\Log::writeException($Exception);
+            }
+        }
 
         /* @var $Product QUI\ERP\Products\Product\Product */
         foreach ($products as $Product) {
 
+            $btnLabel = QUI::getLocale()->get(
+                'quiqqer/product-bricks',
+                'brick.control.productSlider.buyNow'
+            );
+
             $text = '<p class="slide-product-description">' . $Product->getDescription() . '</p>';
-            $text .= '<p><button class="btn btn-primary btn-large">Jetzt kaufen</button>';
+            $text .= '<p><button class="btn btn-primary btn-large">' . $btnLabel . '</button>';
 
             $Slider->addSlide(
                 $Product->getImage()->getUrl(),
@@ -84,141 +94,12 @@ class ProductSlider extends QUI\Control
                 'left',
                 $Product->getUrl()
             );
-
         }
 
-
-        /* @var $Product QUI\ERP\Products\Product\Product */
-        /*$Product = $products[0];
-        $Product->getImage();
-        $Product->getUrl();
-        $Product->getCategory();
-
-        $this->Slider->addSlide();*/
-
-
         $Engine->assign([
-            'this'   => $this,
             'Slider' => $Slider
         ]);
 
-
         return $Engine->fetch($this->getAttribute('template'));
-    }
-
-    /**
-     * Add a slide for the desktop view
-     *
-     * @param string $image - image.php URL to an image
-     * @param string $left - optional, left text
-     * @param string $right - optional, right text
-     * @param string|bool $type - optional, not exists, but we are from PromosliderWallpaper and AbstractPromoslider
-     * @param string $url - index.php? or extern url
-     */
-    public function addSlide($image, $left = '', $right = '', $type = false, $url = '')
-    {
-        $this->desktopSlides[] = $this->checkSlideParams($image, $left, $right, $url);
-    }
-
-    /**
-     * Add a slide for the mobile view
-     *
-     * @param string $image - image.php URL to an image
-     * @param string $left - optional, left text
-     * @param string $right - optional, right text
-     * @param string $url - index.php? or extern url
-     */
-    public function addMobileSlide($image, $left = '', $right = '', $url = '')
-    {
-        $this->mobileSlides[] = $this->checkSlideParams($image, $left, $right, $url);
-    }
-
-    /**
-     * Add a slide for the mobile view
-     *
-     * @param string $image - image.php URL to an image
-     * @param string $left - Left text
-     * @param string $right - Right text
-     * @param string $url - index.php? or extern url
-     * @return array
-     */
-    protected function checkSlideParams($image, $left = '', $right = '', $url = '')
-    {
-        if (Utils::isMediaUrl($image)) {
-            try {
-                $Image = Utils::getMediaItemByUrl($image);
-
-                if (Utils::isImage($Image)) {
-                    $image = $Image;
-                } else {
-                    $image = false;
-                }
-            } catch (QUI\Exception $Exception) {
-                QUI\System\Log::addDebug($Exception->getMessage());
-                $image = false;
-            }
-        } else {
-            $image = false;
-        }
-
-        return [
-            'image' => $image,
-            'left'  => $left,
-            'right' => $right,
-            'url'   => $url
-        ];
-    }
-
-    /**
-     * Parse slide params and add the slide
-     *
-     * @param mixed $slides
-     * @param string $type
-     */
-    protected function parseSlides($slides, $type = 'desktop')
-    {
-        if (empty($slides)) {
-            return;
-        }
-
-        // desktop slides
-        if (is_string($slides)) {
-            $slides = json_decode($slides, true);
-        }
-
-        if (!is_array($slides)) {
-            return;
-        }
-
-        $attributes = ['image', 'left', 'right', 'url'];
-
-        foreach ($slides as $slide) {
-            foreach ($attributes as $attribute) {
-                if (!isset($slide[$attribute])) {
-                    $slide[$attribute] = false;
-                }
-            }
-
-            switch ($type) {
-                case 'desktop':
-                    $this->addSlide(
-                        $slide['image'],
-                        $slide['left'],
-                        $slide['right'],
-                        'desktop',
-                        $slide['url']
-                    );
-                    break;
-
-                case 'mobile':
-                    $this->addMobileSlide(
-                        $slide['image'],
-                        $slide['left'],
-                        $slide['right'],
-                        $slide['url']
-                    );
-                    break;
-            }
-        }
     }
 }
