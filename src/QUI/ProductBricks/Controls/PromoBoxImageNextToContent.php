a80e2d46a164bd4be575d90ab35afbc0deb918b5
michael.daniel <michael@pcsg.de>
2019-01-22 15:46:47 +0100

    feat: New brick is added (Forgotten files).

---
+++ src/QUI/ProductBricks/Controls/PromoBoxImageNextToContent.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file contains QUI\ProductBricks\Controls\PromoBoxImageNextToContent
+ */
+
+namespace QUI\ProductBricks\Controls;
+
+use QUI;
+
+/**
+ * Class CategoryBox
+ *
+ * @package QUI\Bricks\Controls
+ */
+class PromoBoxImageNextToContent extends QUI\Control
+{
+    /**
+     * constructor
+     *
+     * @param array $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        parent::__construct($attributes);
+
+        // default options
+        $this->setAttributes([
+            'class'           => 'quiqqer-productbricks-promoboxImageNextToContent',
+            'nodeName'        => 'section',
+            'backgroundColor' => '#fff',
+            'minHeight'       => 400,
+            'contentPosition' => 'center',
+            'image'           => false,
+            'url'             => false,
+            'target'          => '_self',
+            'template'        => dirname(__FILE__) . '/PromoBoxImageNextToContent.html'
+        ]);
+
+        $this->addCSSFile(dirname(__FILE__) . '/PromoBoxImageNextToContent.css');
+    }
+
+    public function getBody()
+    {
+        $Engine = QUI::getTemplateManager()->getEngine();
+
+        $Engine->assign([
+            'this'            => $this,
+            'minHeight'       => $this->getAttribute('minHeight'),
+            'image'           => $this->getAttribute('image'),
+            'contentPosition' => $this->getAttribute('contentPosition'),
+            'url'             => $this->getAttribute('url'),
+            'target'          => $this->getAttribute('target'),
+            'backgroundColor' => $this->getAttribute('backgroundColor'),
+            'layout'          => 'layout-' . $this->getAttribute('layout')
+        ]);
+
+        return $Engine->fetch($this->getAttribute('template'));
+    }
+}
